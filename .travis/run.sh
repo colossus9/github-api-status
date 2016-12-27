fa7f99193e951870a2769d08a817c0a9ab10852c
James Garcia <james.rob.garcia@hpe.com>
2016-12-27 13:37:11 -0600

    Added API Check Logic

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python apitest.py
+python github-api-check.py
---
+++ github-api-check.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+print 'Checking GitHub API Status...'
+
+# Load appropriate HTTP libs
+try:
+    from urllib.request import Request, urlopen, HTTPError, URLError     # Python 3
+    from urllib.parse import urlparse, urlsplit
+except:
+    from urllib2 import Request, urlopen, HTTPError, URLError            # Python 2
+    from urlparse import urlparse, urlsplit
+    
+# Build Request Object
+url = 'https://status.github.com/api/status.json'
+print 'Checking url \'' + str(url) + '\''
+request = Request()
+
+# Check the response
+try:
+    response = urlopen(request)    
+    print str(response)
+except HTTPError as e:
+    print 'ERROR: HTTPError Exception thrown; ' + str(e)
+    sys.exit(code)
+except URLError as e:
+    print 'ERROR: URLError Exception thrown; ' + str(e)
+    sys.exit(code)
+else:
+    print 'GitHub API Check PASSED'
